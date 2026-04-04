2720c280de7bec1ad7c4159158a3f1d10c0b8274
pratapcode <yadavsugam347@gmail.com>
2026-04-04 23:38:44 +0545

    feat: made store and reducer for this project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
 import './App.css'
+import authService from './appwrite/auth'
+import {login, logout } from './store/authSlice'
+import { Footer, Header } from './components'
+
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
 
-  return (
-    <>
-      <h1 className='bg-red-900 text-4xl'>Hello </h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then( (userData) => {
+      if(userData){
+        dispatch(login({userData}))
+      } else{
+        dispatch(logout())
+      }
+    })
+    .finally( () => setLoading(false))
+  }, [])
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+        TODO  {/* <outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : (null)
 }
 
 export default App
--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -43,7 +43,7 @@ export class AuthService {
     try {
         return await this.account.get();
     } catch (error) {
-        console.error("Appwrute service :: getCurrentUser :: error", error)
+        console.error("Appwrite service :: getCurrentUser :: error", error)
     }
     return null;
   }
